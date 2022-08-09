62d81cb31460a88701f8b3eedd7f020d896981f6
aisyahnur11 <110007516+aisyahnur11@users.noreply.github.com>
2022-08-09 11:40:46 +0700

    Add files via upload

---
+++ tambah3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="proses_tambah.php">
+        <P>nama <input name="nama" type="text"></P>
+        <p>kelas <input name="kelas" type="text"></p>
+        <button type="submit">ok</button>
+
+    </form>
+</body>
+</html>
